76e38f09246ed6bb17988cecce1816a2465f28fc
dev-bhatt2009 <devbhatt5617@gmail.com>
2026-05-23 10:12:26 +0530

    Create 04_variables.py

---
+++ 04_variables.py
@@ -0,0 +1,8 @@
+a=20
+b=10
+
+print("Sum of these two numbers is:",a+b)
+print("substraction of these two numbers is:",a-b)
+print("Multiplication of these two numbers is:",a*b)
+print("Division of these two numbers is:",a/b)
+print("Modulo of these two numbers:",a%b)
