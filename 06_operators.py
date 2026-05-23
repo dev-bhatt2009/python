7639095204269d2e9d698d6cc8c1a766019ad6b5
dev-bhatt2009 <devbhatt5617@gmail.com>
2026-05-23 10:31:08 +0530

    Create 06_operators.py

---
+++ 06_operators.py
@@ -0,0 +1,44 @@
+# Arithmetic operators
+
+a=56
+b=44
+print(a+b)  #output of this operator is 100
+
+
+#Assignment operators
+
+a=45-25  #Assign 45-25 in a
+print(a)  #Output of this operator is 20
+b=8
+b+=5  #increment the value of b by 5 and assign it to b
+print(b)
+
+
+#Comparison operators
+
+c= 5==5  #5==5 ko c mein assign kiya
+print(c)  #output of this operator is True
+d = 10=20
+print(d)  #output of this operator is False
+
+
+#logical operators
+
+e=True or False
+
+#truth table of 'or'
+
+print("True or False is",True or False)
+print("True or True is",True or True)
+print("False or True is",False or True)
+print("False or False is",False or False)
+
+#truth table of 'and'
+
+print("True and False is",True and False)
+print("True and True is",True and True)
+print("False and True is",False and True)
+print("False and False is",False and False)
+
+print(not(True))   #output is False
+print(not(False))  #output is True
